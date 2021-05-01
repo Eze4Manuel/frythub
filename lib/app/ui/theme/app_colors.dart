3f939f53ec30f5ef2da6aca3e7d8778d430bb8e6
nnamdi <nnamsok@gmail.com>
2021-05-01 23:10:28 +0100

    app font and color include

---
+++ lib/app/ui/theme/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color appPrimaryColor = Color.fromRGBO(249, 89, 45, 1);
+  static const Color appColor1 = Color.fromRGBO(8, 47, 46, 1);
+  static const Color appColorBlack = Color.fromRGBO(97, 116, 141, 1);
+  static const Color appColorWhite = Color.fromRGBO(255, 188, 168, 1);
+
+  // Other Colors
+  static const Color color1 = Color.fromRGBO(255, 232, 226, 1);
+  static const Color color2 = Color.fromRGBO(248, 250, 252, 1);
+  static const Color color3 = Color.fromRGBO(201, 213, 226, 1);
+  static const Color color4 = Color.fromRGBO(221, 195, 143, 1);
+}
---
+++ lib/app/utils/dimensions.dart
@@ -0,0 +1,7 @@
+class Dimens {
+  Dimens._();
+
+  //for all screens
+  static const double horizontal_dimen = 32.0;
+// static const double vertical_dimen = 12.0;
+}
